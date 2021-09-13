b911696b346bb24e527e31f38fe5f84ef5485a3e
capitalist42 <capitalist42@protonmail.ch>
2021-09-13 18:46:27 +0800

    upgrade UI code to use redux async actions

--- src/screens/import-seed-phrase.tsx
+++ src/screens/import-seed-phrase.tsx
@@ -1,20 +1,63 @@
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
+import {Alert} from 'react-native';
 import {useForm, Controller} from 'react-hook-form';
-import {VStack, Button, TextArea, FormControl} from 'native-base';
-import {DirectSecp256k1HdWallet} from '@cosmjs/proto-signing';
+import * as SecureStore from 'expo-secure-store';
+import {
+  VStack,
+  Button,
+  TextArea,
+  Modal,
+  Input,
+  FormControl,
+} from 'native-base';
+import {useAppDispatch} from '../hooks';
+import {importWalletAsync, saveNewWalletAsync} from '../features/wallet/walletSlice';
 
 const ImportSeedPhraseScreen = () => {
-  const {control, handleSubmit} = useForm({
+  const dispatch = useAppDispatch();
+  const [showEncryptWalletModal, setShowEncryptWalletModal] = useState(false);
+  const initalShowEncryptWalletModalRef = useRef();
+  const [showPassword, setShowPassword] = useState(false);
+  const {control: controlImportSeed, handleSubmit: handleImportSeedSubmit} =
+    useForm({
+      defaultValues: {
+        seedPhrase: '',
+      },
+    });
+  const {
+    control: controlEncryptWallet,
+    handleSubmit: handleEncryptWalletSubmit,
+  } = useForm({
     defaultValues: {
-      seedPhrase: '',
+      password: '',
     },
   });
 
-  const onSubmit = async (data: {seedPhrase: string}) => {
-    const wallet = await DirectSecp256k1HdWallet.fromMnemonic(data.seedPhrase);
-    const [firstAccount] = await wallet.getAccounts();
-    console.log(firstAccount);
-    console.log(firstAccount.pubkey);
+  const onSubmitImportSeedPhraseForm = async (data: {seedPhrase: string}) => {
+    dispatch(importWalletAsync(data.seedPhrase))
+      .unwrap()
+      .then((originalPromiseResult) => {
+        console.debug(originalPromiseResult);
+        setShowEncryptWalletModal(true);
+      })
+      .catch((rejectedValueOrSerializedError) => {
+        Alert.alert('Error ', rejectedValueOrSerializedError.message, [
+          {text: 'OK chillout'},
+        ]);
+      });
+  };
+
+  const onSubmitEncrypWalletForm = async (data: {password: string}) => {
+    dispatch(saveNewWalletAsync(data.password))
+      .unwrap()
+      .then((originalPromiseResult) => {
+        console.debug(originalPromiseResult);
+      })
+      .catch((rejectedValueOrSerializedError) => {
+        Alert.alert('Error ', rejectedValueOrSerializedError.message, [
+          {text: 'OK chillout'},
+        ]);
+      });
   };
 
   return (
@@ -22,7 +65,7 @@ const ImportSeedPhraseScreen = () => {
       <FormControl isRequired>
         <FormControl.Label>Enter a 24-word seed phrase</FormControl.Label>
         <Controller
-          control={control}
+          control={controlImportSeed}
           name="seedPhrase"
           render={({
             field: {onChange, value},
@@ -30,17 +73,75 @@ const ImportSeedPhraseScreen = () => {
             formState,
           }) => (
             <TextArea
-              placeholder="TextArea"
+              placeholder=""
               onChangeText={(val) => onChange(val)}
               defaultValue={value}
+              numberOfLines={4}
+              autoCapitalize="none"
+              secureTextEntry={true}
             />
           )}
         />
       </FormControl>
 
-      <Button onPress={handleSubmit(onSubmit)} colorScheme="pink">
+      <Button
+        onPress={handleImportSeedSubmit(onSubmitImportSeedPhraseForm)}
+        colorScheme="pink">
         Submit
       </Button>
+      <Modal
+        isOpen={showEncryptWalletModal}
+        onClose={() => setShowEncryptWalletModal(false)}>
+        <Modal.Content maxWidth="400px">
+          <Modal.CloseButton />
+          <Modal.Header>Encrypt Wallet</Modal.Header>
+          <Modal.Body>
+            <FormControl isRequired>
+              <Controller
+                control={controlEncryptWallet}
+                name="password"
+                render={({
+                  field: {onChange, value},
+                  fieldState: {invalid, isTouched, isDirty, error},
+                  formState,
+                }) => (
+                  <Input
+                    mt={4}
+                    ref={initalShowEncryptWalletModalRef}
+                    defaultValue={value}
+                    type={showPassword ? 'text' : 'password'}
+                    onChangeText={(val) => onChange(val)}
+                    InputRightElement={
+                      <Button
+                        ml={1}
+                        roundedLeft={0}
+                        roundedRight="md"
+                        onPress={() => setShowPassword(!showPassword)}>
+                        {showPassword ? 'Hide' : 'Show'}
+                      </Button>
+                    }
+                    placeholder="Password"
+                  />
+                )}
+              />
+            </FormControl>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button.Group variant="ghost" space={2}>
+              <Button
+                onPress={handleEncryptWalletSubmit(onSubmitEncrypWalletForm)}>
+                Confirm
+              </Button>
+              <Button
+                onPress={() => {
+                  setShowEncryptWalletModal(false);
+                }}>
+                Cancel
+              </Button>
+            </Button.Group>
+          </Modal.Footer>
+        </Modal.Content>
+      </Modal>
     </VStack>
   );
 };
