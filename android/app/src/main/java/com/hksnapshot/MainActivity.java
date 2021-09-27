ee185adaad9088a2d187f3d7895eab072603109d
capitalist42 <capitalist42@protonmail.ch>
2021-09-27 16:53:34 +0800

    upgrade react-navigation

--- android/app/src/main/java/com/hksnapshot/MainActivity.java
+++ android/app/src/main/java/com/hksnapshot/MainActivity.java
@@ -1,5 +1,5 @@
 package com.hksnapshot;
-
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
@@ -12,4 +12,10 @@ public class MainActivity extends ReactActivity {
   protected String getMainComponentName() {
     return "hksnapshot";
   }
+  
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
+  }
+
 }
