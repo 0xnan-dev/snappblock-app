33178ae24de1d00fb15a79c7caeffa40a10c13a6
0xKChau <walterchau0416@gmail.com>
2021-09-27 18:37:16 +0800

    Update signIscnTxn and add @likecoin/iscn-js pkg

--- src/screens/snapshot.tsx
+++ src/screens/snapshot.tsx
@@ -1,28 +1,28 @@
-import React, { useRef } from 'react';
-import {Box, Button, Text} from 'native-base';
-import { RNCamera } from 'react-native-camera';
-import { useIpfs } from '../navigation/ipfs-http-client';
-import { signISCNTxn } from '../lib/signIscnTxn';
+/* eslint-disable no-shadow */
+/* eslint-disable react-native/no-inline-styles */
+import React, {useRef} from 'react';
+import {Box, Button} from 'native-base';
+import {RNCamera} from 'react-native-camera';
+import {useIpfs} from '../navigation/ipfs-http-client';
+import {signISCNTxn} from '../lib/signIscnTxn';
 
 const SnapshotScreen = () => {
-  const cameraRef:any = useRef(null);
-  const { client } = useIpfs();
-
-  console.log('client', client)
+  const cameraRef: any = useRef(null);
+  const {client} = useIpfs();
 
   const takePicture = async () => {
     try {
       if (cameraRef) {
-        const options = { quality: 0.5, base64: true };
+        const options = {quality: 0.5, base64: true};
         const data = await cameraRef!.current.takePictureAsync(options);
-        await fetchPhoto(data, client)
+        await fetchPhoto(data, client);
       }
     } catch (error) {
       console.error(error);
     }
   };
 
-  const fetchPhoto = async (photo:any, client:any) => {
+  const fetchPhoto = async (photo: any, client: any) => {
     // console.debug('photo', photo);
     try {
       const response = await fetch(photo.uri);
@@ -47,8 +47,7 @@ const SnapshotScreen = () => {
       const url = `https://ipfs.io/ipfs/${result?.cid?.toString()}`;
       console.debug(`Url --> ${url}`);
 
-      await signIscn(result?.cid?.toString())
-
+      await signIscn(result?.cid?.toString());
     } catch (error) {
       console.error('Demo App .add photo', {error});
     }
@@ -57,57 +56,61 @@ const SnapshotScreen = () => {
   const signIscn = async (cid: string) => {
     try {
       const payload = {
-        title: '0xNan Demo',
-        description: 'This is a description',
-        tagsString: 'article',
-        url: 'https://nnkken.github.io/post/recursive-relation/',
-        license: 'https://creativecommons.org/licenses/by/4.0',
+        contentFingerprints: [
+          'hash://sha256/9564b85669d5e96ac969dd0161b8475bbced9e5999c6ec598da718a3045d6f2e',
+        ],
         ipfsHash: `ipfs://${cid}`,
-        // fileSHA256: 'hash://sha256/9564b85669d5e96ac969dd0161b8475bbced9e5999c6ec598da718a3045d6f2e',
-        authorNames: '0xNan',
-        authorUrls: 'http://github.com/0xNan',
+        stakeholders: [
+          {
+            entity: {
+              '@id': 'did:cosmos:12zu5qe7mdkh45e3qhq768tpzwd90q8rjgp88cu',
+              name: '0xNan',
+            },
+            rewardProportion: 95,
+            contributionType: 'http://schema.org/author',
+          },
+        ],
+        type: 'Snapshot',
+        name: '0xNan Demo',
+        usageInfo: 'https://creativecommons.org/licenses/by/4.0',
+        keywords: ['0xNan', 'snapshot'],
+        description: 'This is a description',
       };
-  
-      const address = 'cosmos135h665npk02kn99mayfwr77m44y9ruqq4ua9tc'; // digital ocean node address
-      // const address = "cosmos1uqfcn4ppzlnus9cc5u5jekj2wrjexrs0c5gjs6"; // local node address
-  
-      await signISCNTxn(payload, address);
+
+      await signISCNTxn(payload);
     } catch (err) {
-      console.error(err)
+      console.error(err);
     }
-  }
+  };
 
   return (
     <Box>
-        <RNCamera
-          ref={cameraRef}
-          type={RNCamera.Constants.Type.back}
-          flashMode={RNCamera.Constants.FlashMode.on}
-          androidCameraPermissionOptions={{
-            title: 'Permission to use camera',
-            message: 'We need your permission to use your camera',
-            buttonPositive: 'Ok',
-            buttonNegative: 'Cancel',
-          }}
-          androidRecordAudioPermissionOptions={{
-            title: 'Permission to use audio recording',
-            message: 'We need your permission to use your audio',
-            buttonPositive: 'Ok',
-            buttonNegative: 'Cancel',
-          }}
-          onGoogleVisionBarcodesDetected={({ barcodes }) => {
-            console.debug(barcodes);
-          }}
-        />
-        <Box style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
-          <Button
-            marginY="10px"
-            colorScheme="primary"
-            onPress={takePicture}>
-            SNAP
-          </Button>
-        </Box>
+      <RNCamera
+        ref={cameraRef}
+        type={RNCamera.Constants.Type.back}
+        flashMode={RNCamera.Constants.FlashMode.on}
+        androidCameraPermissionOptions={{
+          title: 'Permission to use camera',
+          message: 'We need your permission to use your camera',
+          buttonPositive: 'Ok',
+          buttonNegative: 'Cancel',
+        }}
+        androidRecordAudioPermissionOptions={{
+          title: 'Permission to use audio recording',
+          message: 'We need your permission to use your audio',
+          buttonPositive: 'Ok',
+          buttonNegative: 'Cancel',
+        }}
+        onGoogleVisionBarcodesDetected={({barcodes}) => {
+          console.debug(barcodes);
+        }}
+      />
+      <Box style={{flex: 0, flexDirection: 'row', justifyContent: 'center'}}>
+        <Button marginY="10px" colorScheme="primary" onPress={takePicture}>
+          SNAP
+        </Button>
       </Box>
+    </Box>
   );
 };
 export default SnapshotScreen;
