962a1131038a3d8f7c0e5390eddf444f5d2c1e73
capitalist42 <capitalist42@protonmail.ch>
2021-11-18 18:34:42 +0800

    show splash screen when restoring wallet in background

--- src/navigation/app-stack-navigator.tsx
+++ src/navigation/app-stack-navigator.tsx
@@ -1,60 +1,70 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import * as SecureStore from 'expo-secure-store';
 import {DEFAULT_WALLET_SERIALIZATION_SECURE_STORE_KEY} from '../features/wallet/walletSlice';
-
+import SplashScreen from '../screens/splash-screen';
 import WelcomeScreen from '../screens/welcome';
 import NewWalletScreen from '../screens/new-wallet';
 import ImportSeedPhraseScreen from '../screens/import-seed-phrase';
 import HomeTab, {PhotoStack} from './homeTab';
 
-import {selectOptionalWalletSerialization} from '../features/wallet/walletSlice';
-import {useAppSelector, useAppDispatch} from '../hooks';
+// import {selectOptionalWalletSerialization} from '../features/wallet/walletSlice';
+import {useAppDispatch} from '../hooks';
 
 const Stack = createStackNavigator();
 
 function AppStackNavigator() {
   const dispatch = useAppDispatch();
-  const walletSerialization = useAppSelector(selectOptionalWalletSerialization);
+  const [restoringWalletSeriazation, setRestoringWalletSeriazation] =
+    useState(false);
+  const [walletSerialization, setWalletSerialization] = useState(null);
+
   React.useEffect(() => {
     // Fetch the token from storage then navigate to our appropriate place
     const bootstrapAsync = async () => {
       let serialization;
       try {
+        setRestoringWalletSeriazation(true);
         serialization = await SecureStore.getItemAsync(
           DEFAULT_WALLET_SERIALIZATION_SECURE_STORE_KEY,
         );
       } catch (e) {
         // Restoring token failed
+        dispatch({type: 'restoreSerliazationFailed', serialization: null});
         console.debug(e);
       }
-      console.debug('serialization', serialization);
+      setWalletSerialization(serialization);
       dispatch({type: 'restoreSerialization', serialization: serialization});
+      setRestoringWalletSeriazation(false);
     };
 
     bootstrapAsync();
   }, [dispatch]);
 
   return (
-    <Stack.Navigator
-      initialRouteName="Welcome"
-      screenOptions={{gestureEnabled: false}}>
-      {walletSerialization ? (
-        <>
-          <Stack.Screen name="HK SnapShot" component={HomeTab} />
-          <Stack.Screen name="PhotoStack" component={PhotoStack} />
-        </>
+    <>
+      {restoringWalletSeriazation ? (
+        <SplashScreen />
       ) : (
-        <>
-          <Stack.Screen name="Welcome" component={WelcomeScreen} />
-          <Stack.Screen name="Seed Phrase" component={NewWalletScreen} />
-          <Stack.Screen
-            name="Import Seed Phrase"
-            component={ImportSeedPhraseScreen}
-          />
-        </>
+        <Stack.Navigator screenOptions={{gestureEnabled: false}}>
+          {walletSerialization ? (
+            <>
+              <Stack.Screen name="HK SnapShot" component={HomeTab} />
+              <Stack.Screen name="PhotoStack" component={PhotoStack} />
+            </>
+          ) : (
+            <>
+              <Stack.Screen name="Welcome" component={WelcomeScreen} />
+              <Stack.Screen name="Seed Phrase" component={NewWalletScreen} />
+              <Stack.Screen
+                name="Import Seed Phrase"
+                component={ImportSeedPhraseScreen}
+              />
+            </>
+          )}
+        </Stack.Navigator>
       )}
-    </Stack.Navigator>
+    </>
   );
 }
 
