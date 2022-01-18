ac2a60e15c8b33aae8630353e38f9a84b8e3e4ef
Joseph Cheng <indiejoseph@gmail.com>
2022-01-18 08:09:19 +0800

    chore: add sentry config

--- app.config.ts
+++ app.config.ts
@@ -34,6 +34,11 @@ export default function ({ config }: any) {
       postPublish: [
         {
           file: 'sentry-expo/upload-sourcemaps',
+          config: {
+            organization: process.env.SENTRY_ORG,
+            project: process.env.SENTRY_PROJECT,
+            authToken: process.env.SENTRY_AUTH_TOKEN,
+          },
         },
       ],
     },
