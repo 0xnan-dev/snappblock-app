5d94696b627654b8c494289bfe7fdf25ee44863b
Joseph Cheng <indiejoseph@gmail.com>
2022-01-25 12:12:01 +0800

    chore: remove old files

--- src/screens/splash.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {Box} from 'native-base';
-
-const SplashScreen = () => {
-  return (
-    <Box flex={1} bg="#fff" alignItems="center" justifyContent="center"></Box>
-  );
-};
-export default SplashScreen;
